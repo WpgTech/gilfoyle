610f2b84549177341a1606c89f830de84364ddb2
Jordan Neufeld <myjc.niv@gmail.com>
2016-09-19 15:49:35 -0500

    Move docker pull out of systemd script to deploy steps instead. Remove deploy shell script and let the fabfile take care of it all.

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,15 @@ from fabric.contrib.console import confirm
 env.forward_agent = True
 
 def deploy():
+    #upload the docker image we built
+    print("Logging into dockerhub")
+    local(docker login -u $DOCKER_USER -p $DOCKER_PASS)
+    print("Tagging image gilfoyle as wpgtech/gilfoyle:latest")
+    local(docker tag gilfoyle wpgtech/gilfoyle:latest)
+    print("Pushing wpgtech/gilfoyle:latest to dockerhub")
+    local(docker push wpgtech/gilfoyle:latest)
+    
+    #deploy on remote server
     result=run("uname -r")
 
     if result.succeeded:
@@ -16,10 +25,11 @@ def deploy():
     else:
       print "[FAILED] Could *not* ssh into server "+env.host_string+"! Please fix the auto-ssh issue. Exiting!";
       sys.exit(1)
-
-    run("whoami")
-    
+   
     local_dir = os.path.dirname(os.path.realpath(__file__))
     print("Copying docker-compose.yml to server")
     put(local_dir + "/docker-compose.yml", '/opt/hubot/gilfoyle/')
+
+    with cd('/opt/hubot/gilfoyle'):
+      run('/usr/local/bin/docker-compose pull')
     run('sudo systemctl restart gilfoyle.service')
